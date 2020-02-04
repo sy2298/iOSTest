93dd1d9410bd04f8e64dd04797610d536ce4f1e3
Lee <sy2298@naver.com>
2020-02-04 16:17:21 +0900

    Initial Commit

---
+++ Pin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pin
+//
+//  Created by Emily Nan on 2020/02/04.
+//  Copyright © 2020 Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
